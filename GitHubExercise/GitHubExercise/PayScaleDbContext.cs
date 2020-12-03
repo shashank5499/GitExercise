9bf032d06d15ac0a485e2a0f247c58d6bc092114
Shashank Thakur <shashank.thakur.1848816@gmail.com>
2020-12-03 13:21:43 +0530

    First Commit

---
+++ GitHubExercise/GitHubExercise/PayScaleDbContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace GitHubExercise
+{
+    public partial class PayScaleDbContext : DbContext
+    {
+        public PayScaleDbContext()
+        {
+        }
+
+        public PayScaleDbContext(DbContextOptions<PayScaleDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Payscale> Payscales { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=ALIENWARE;Database=PayScaleDb;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Payscale>(entity =>
+            {
+                entity.HasKey(e => e.PayBand)
+                    .HasName("PK__Payscale__66B0F53F08659C49");
+
+                entity.ToTable("Payscale");
+
+                entity.Property(e => e.PayBand).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ GitHubExercise/GitHubExercise/Payscale.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace GitHubExercise
+{
+    public partial class Payscale
+    {
+        public string PayBand { get; set; }
+        public double? BasicSalary { get; set; }
+    }
+}
